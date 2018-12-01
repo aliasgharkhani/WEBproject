b5f6dcba054d6394a88d7e3e7f8ff7f7ee5dc2fb
HEffatpanah <h.effatpanah@gmail.com>
2018-12-01 19:35:27 +0330

    commit 2

---
+++ build/precache-manifest.16da1223b4db55fa4b9e3d95fa1ac915.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3caadf2bae6b9be8c8ab",
+    "url": "/static/css/main.2ef1ff1c.chunk.css"
+  },
+  {
+    "revision": "3caadf2bae6b9be8c8ab",
+    "url": "/static/js/main.3caadf2b.chunk.js"
+  },
+  {
+    "revision": "28426e4a7817f873f572",
+    "url": "/static/js/1.28426e4a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0c34b4e8f2baef6c0458b8948440c134",
+    "url": "/index.html"
+  }
+];
